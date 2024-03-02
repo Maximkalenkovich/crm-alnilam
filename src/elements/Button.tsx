4823e6fa2b50e48f638a4ed60b30883a81f91685
Maximkalenkovich <peterson.maxim@mail.ru>
2024-03-02 23:07:00 +0300

    add bll for product

---
+++ src/elements/Button.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+
+
+
+type ButtonType = {
+    title:string
+    onClick:()=>void
+}
+
+
+export const Button = (props:ButtonType)=>{
+    return(
+
+            <button onClick={props.onClick}>{props.title}</button>
+
+    )
+}
